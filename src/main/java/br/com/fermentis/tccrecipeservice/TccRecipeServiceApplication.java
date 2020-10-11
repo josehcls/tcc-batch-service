46c2026cbe53ffc9ee22943158618357b0e33c52
Jose Leopoldo <josehcls@gmail.com>
2020-10-11 19:29:52 -0300

    Spring init

---
+++ src/main/java/br/com/fermentis/tccrecipeservice/TccRecipeServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fermentis.tccrecipeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccRecipeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccRecipeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fermentis/tccrecipeservice/TccRecipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fermentis.tccrecipeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccRecipeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
